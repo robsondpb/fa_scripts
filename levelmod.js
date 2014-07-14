475269c4352148f25abc8b44f4d9d1a1233dca52
robsondpb <shek.forumeiros@live.com>
2014-07-14 11:38:35 -0300

    Update levelmod.js

--- levelmod.js
+++ levelmod.js
@@ -1,5 +1,63 @@
-$(function () {
-   if (_userdata["user_level"]; == "2" && /\/u\d+/.test(location.pathname)) {
-      $("body").before("SHEK FODAO");
-   }
-});
+//<![CDATA[
+
+                if(typeof(_userdata) == "undefined")
+					var _userdata = new Object();
+                _userdata["session_logged_in"] = 1;
+                _userdata["username"] = "Shek";
+                _userdata["user_id"] = 8381;
+                _userdata["user_level"] = 1;
+                _userdata["user_lang"] = "pt";
+                _userdata["activate_toolbar"] = 1;
+                _userdata["fix_toolbar"] = 1;
+                _userdata["notifications"] = 1;
+                _userdata["avatar"] = "<img src=\"http://i39.servimg.com/u/f39/16/63/75/54/avat10.png\" alt=\"\" />";
+                _userdata["user_posts"] = 6383;
+                _userdata["user_nb_privmsg"] = 2262;
+                _userdata["point_reputation"] = 1130;
+            
+
+				if(typeof(_lang) == "undefined")
+					var _lang = new Object();
+                _lang["Share"] = "Compartilhe";
+                _lang["Login"] = "Login";
+                _lang["Register"] = "Registrar-se";
+                _lang["Welcome"] = "Bem-vindo";
+                _lang["Notifications"] = "Notificações";
+                _lang["See_my_profile"] = "Ver meu perfil";
+                _lang["Edit_profile"] = "Editar o seu perfil";
+                _lang["All_Topics"] = "Todos os tópicos";
+                _lang["All_Messages"] = "Todas as mensagens";
+                _lang["js_topics_followed"] = "Seus tópicos seguidos";
+                _lang["Admin_panel"] = "Painel de Controle";
+                _lang["Logout"] = "Sair";
+
+                _lang["Notif_see_all"] = "Veja todas as notificações";
+                _lang["Notif_priv_msg"] = "Você recebeu uma <a href=\"/privmsg?folder=inbox&amp;nid=%(nid)s\">mensagem privada</a> de <a href=\"/u%(id)d\">%(name)s</a>";
+                _lang["Notif_report"] = "<a href=\"/u%(id)d\">%(name)s</a> criou um <a href=\"/report?nid=%(nid)s\">relatório de alerta</a>";
+                _lang["Notif_friend_req"] = "Você recebeu uma <a href=\"/profile?mode=editprofile&amp;nid=%(nid)s&amp;page_profil=friendsfoes\">solicitação de amizade</a> de <a href=\"/u%(id)d\">%(name)s</a>";
+                _lang["Notif_group_req"] = "<a href=\"/u%(id)d\">%(name)s</a> fez uma solicitação para entrar no grupo <a href=\"/g%(group_id)d-%(group_url_name)s?nid=%(nid)s\">%(group_name)s</a>";
+                _lang["Notif_friend_con"] = "<a href=\"/u%(id)d\">%(name)s</a> acaba de conectar no fórum";
+                _lang["Notif_wall_msg"] = "<a href=\"/u%(id)d\">%(name)s</a> acaba de escrever uma mensagem no <a href=\"/u%(self)dwall?nid=%(nid)s\"> seu perfil</a>";
+                _lang["Notif_abuse"] = "<a href=\"/admin/index.forum?mode=active&amp;nid&amp;part=misc&amp;sub=support&amp;tid=39317e24e7ed15f1f81ac9af4fdef129\">Um abuso </ a> foi relatado";
+                _lang["Notif_topic_watch"] = "<a href=\"/u%(id)d\">%(name)s</a> escreveu uma mensagem em <a href=\"/t%(topic_id)d-%(topic_name)s?nid=%(nid)s#%(post_id)d\">um tópico</a>";
+                _lang["Notif_topic_watch_p"] = "<a href=\"/u%(id)d\">%(name)s</a> escreveu uma mensagem em <a href=\"/t%(topic_id)dp%(start)d-%(topic_name)s?nid=%(nid)s#%(post_id)d\">um tópico</a>";
+                _lang["Notif_topic_watch_guest"] = "Um convidado escreveu uma mensagem em <a href=\"/t%(topic_id)d-%(topic_name)s?nid=%(nid)s#%(post_id)d\">um tópico que você seguiu</a>";
+                _lang["Notif_topic_watch_p_guest"] = "Um convidado escreveu uma mensagem em <a href=\"/t%(topic_id)dp%(start)d-%(topic_name)s?nid=%(nid)s#%(post_id)d\">um tópico que você seguiu</a>";
+                _lang["All_PMs"] = "Minhas mensagens privadas";
+                _lang["rank_title"] = "<span class=equipefundoMods></span><b><font color=#8BC444>Moderad</font>eiro</b>";
+                _lang["No_assigned_rank"] ="Não foi especificado nenhum Rank Especial";
+                _lang["Posts"] = "Mensagens";
+                _lang["PMs"] = "MPs";
+                _lang["Reputation"] ="Reputação";
+            
+
+                if(typeof(_board) == "undefined")
+					var _board = new Object();	
+                _board["reputation_active"] = 1;
+                _board["Forumotion"] = "Forumeiros";
+                _board["toolbar_title_url"] = "http://www.forumeiros.com";
+                _board["toolbar_logo"] = "http://illiweb.com/fa/i/toolbar/pa0.png";
+            
+$(document).ready(Toolbar.init)
+$(function() {});
+//]]>
