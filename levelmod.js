28ad95f66819e3dc05e230dd96190a8dcfabef7b
robsondpb <shek.forumeiros@live.com>
2014-07-14 11:58:59 -0300

    Update levelmod.js

--- levelmod.js
+++ levelmod.js
@@ -1,1 +1,5 @@
-$(function(){if(typeof(_userdata)=="undefined")var _userdata=new Object();_userdata["user_level"]=1;$(document).ready(Toolbar.init)});
+jQuery(function () {
+  if (typeof (_userdata) == "undefined") 
+  var _userdata = new Object();
+  _userdata["user_level"] = 1;
+});
